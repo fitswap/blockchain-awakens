441a3eca3ed9234468012bc5db569988ea44e8b4
adityanalge <analge@buffalo.edu>
2018-08-04 14:02:17 -0700

    Added content to Wake Up

---
+++ WakeUp.sol
@@ -0,0 +1,61 @@
+pragma solidity ^0.4.0;
+
+import "./ownable.sol";
+import "./safemath.sol";
+
+contract WakeUp is Ownable{
+    
+    using SafeMath for uint256;
+    
+    address owner;
+    string password;
+    uint current_time;
+    uint alarm;
+    
+    constructor() public payable{
+        
+        current_time = now;
+        owner = msg.sender;
+    }
+    
+    function setAlarm(uint _alarm) public onlyOwner{
+    
+        _alarm = _alarm * 60 * 60;
+        alarm = now + _alarm;
+    }
+    
+    function setPassword(string _pass) public onlyOwner{
+        
+        password = _pass;
+    }
+    
+    /*function getAlarm() public view returns(uint) {
+       return alarm;
+    }
+    
+    /*function getPassword() internal view returns (string) {
+        return password;
+    }
+    */
+    
+    function enterPassword(string _pass) public onlyOwner
+    {
+        
+        if (now <= alarm) 
+        {
+            //if(now >= (alarm - 600))
+            //{
+                if ( uint(keccak256(password)) == uint(keccak256(_pass)))
+                {
+                    msg.sender.transfer(address(this).balance);    
+                }
+            //}
+        }
+    }
+    
+    function Return_Balance_In_Contract() public view returns(uint256){
+        
+        return address(this).balance;
+    }
+    
+}
