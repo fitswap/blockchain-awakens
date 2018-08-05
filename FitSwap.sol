167e878be1a29fc0e161e97957b378b83a901301
qiuyiz <qiuyizhang@gmail.com>
2018-08-04 19:36:52 -0700

    add registeruser

--- FitSwap.sol
+++ FitSwap.sol
@@ -9,11 +9,11 @@ contract Fitswap{
         string l_name;
         address user_address;
         UserType user_type;
-        ExtraInfo extra_info;
         uint active;
     }
     
     struct ExtraInfo {
+        address user_address;
         string skills;
         uint avg_rating;
         uint postal_code;
@@ -22,6 +22,7 @@ contract Fitswap{
     enum UserType {NINJA, NOVICE}
     
     mapping(address => User) public total_users_list;
+    mapping(address => ExtraInfo) public extra_info_list;
     mapping(address => User[]) public customer_list;
     mapping(address => uint) public customer_ratings_list;
 }
