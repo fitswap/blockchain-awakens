8048cc32bea5a7dd4525b3380d7ed0dd9e8fe93b
adityanalge <analge@buffalo.edu>
2018-08-05 12:58:21 -0700

    Changes, Last Minute

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,19 @@
-var FitSwap = artifacts.require("./FitSwap.sol");
-var UserEscrow = artifacts.require("./UserEscrow.sol");
 var Customer = artifacts.require("./Customer.sol");
+var FitSwap = artifacts.require("./FitSwap.sol");
+var Migrations = artifacts.require("./Migrations.sol");
+var Ownable = artifacts.require("./Ownable.sol");
+var SafeMath = artifacts.require("./SafeMath.sol");
+var UserEscrow = artifacts.require("./UserEscrow.sol");
+var WakeUp = artifacts.require("./WakeUp.sol");
+var WeightPool= artifacts.require("./WeightPool.sol");
 
 module.exports = function(deployer) {
     deployer.deploy(UserEscrow);
-    deployer.deploy(FitSwap).then( ()=> deployer.deploy(Customer) );
+    deployer.deploy(FitSwap).then( ()=> deployer.deploy(Customer));
+    deployer.deploy(Migrations);
+    deployer.deploy(Ownable);
+    deployer.deploy(SafeMath).then(()=>deployer.deploy(WakeUp));
+	deployer.deploy(UserEscrow);
+    deployer.deploy(WakeUp);
+    deployer.deploy(WeightPool);
 }
